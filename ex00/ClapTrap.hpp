b8e5133a9f848a63ba803c571507a6e3a9c903db
DalexHD <eldalexhd@gmail.com>
2021-11-14 16:31:53 +0000

    feat: add initial template.

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/14 16:29:54 by aborboll          #+#    #+#             */
+/*   Updated: 2021/11/14 16:30:10 by aborboll         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Incluimos las librerias esenciales.
+*/
+#include <iostream>
+#include <string.h>
+
+class ClapTrap
+{
+	private:
+	public:
+};
