234aa9b4ced7486c8a64a5800af469156157a17f
DalexHD <eldalexhd@gmail.com>
2021-11-14 18:49:45 +0000

    feat: add ex00.

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 16:29:54 by aborboll          #+#    #+#             */
-/*   Updated: 2021/11/14 16:30:10 by aborboll         ###   ########.fr       */
+/*   Updated: 2021/11/14 18:21:29 by aborboll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,33 @@
 class ClapTrap
 {
 	private:
+		std::string	name;
+		size_t		hitpoints = 10;
+		size_t		energy_points = 10;
+		size_t		attack_damage = 0;
 	public:
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &name);
+		ClapTrap(void);
+		~ClapTrap();
+		//Actions
+		void		attack(std::string const & target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+		//Getters
+		size_t		getHitPoints(void);
+		size_t		getEnergyPoints(void);
+		size_t		getAttackDamage(void);
+		std::string	getName(void);
+		//Announcements
+		void		born(void);
+		void		outOfFuel(void);
+		void		needsGym(void);
+		void		die();
+		//State management
+		bool		hasEnergy(void);
+		bool		hasMuscles(void);
 };
+
+
+std::ostream & operator<<(std::ostream & stream , ClapTrap & clap);
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 16:28:58 by aborboll          #+#    #+#             */
-/*   Updated: 2021/11/14 16:29:27 by aborboll         ###   ########.fr       */
+/*   Updated: 2021/11/14 18:49:37 by aborboll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 int main(void)
 {
+	ClapTrap	anonymous;
+
+	ClapTrap	elonMusk("elonMusk");
+	ClapTrap	bitcoin("bitcoin");
+	ClapTrap	bitcoinCopy(bitcoin);
+
+	std::cout << std::endl << elonMusk << std::endl << bitcoin << std::endl;
+
+	elonMusk.attack("bitcoin");
+	bitcoin.takeDamage(elonMusk.getAttackDamage());
+	std::cout << std::endl << elonMusk << std::endl << bitcoin << std::endl;
+
+	elonMusk.attack("etherum");
+
+	std::cout << std::endl << bitcoin << std::endl;
+	bitcoin.beRepaired(10);
+	std::cout << std::endl << bitcoin << std::endl;
 	return (0);
 }
